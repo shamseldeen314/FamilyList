ceb0519c726d3d2c73d96eaf79f824917c2cd24b
shamseldeen314 <shamseldeen314@gmail.com>
2023-03-01 12:57:01 +0200

    Added sample data for parents and children

---
+++ src/Data/FamilyData.js
@@ -0,0 +1,33 @@
+const parentsData = [
+  {
+    id: 1,
+    name: "mohamed Sobhy",
+    age: 35,
+    children: [
+      {
+        id: 1,
+        name: "Sobhy Mohame",
+        age: 8,
+      },
+      {
+        id: 2,
+        name: "Adam Smith",
+        age: 5,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 32,
+    children: [
+      {
+        id: 1,
+        name: "Olivia Doe",
+        age: 6,
+      },
+    ],
+  },
+];
+
+export default parentsData;
