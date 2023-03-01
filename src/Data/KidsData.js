f117af2ba02429e3842b5084eaa2c8af1af1c0e2
shamseldeen314 <shamseldeen314@gmail.com>
2023-03-01 13:01:10 +0200

    Added child data as an array of objects

---
+++ src/Data/KidsData.js
@@ -0,0 +1,21 @@
+const Child = [
+  {
+    id: 1,
+    name: "sobhy saleh",
+    address: "kamal ramzy",
+    nationality: "egyption",
+  },
+  {
+    id: 2,
+    name: "ramadan saleh",
+    address: "moaasaset alzkah",
+    nationality: "egyption",
+  },
+  {
+    id: 3,
+    name: "abd alkader saleh",
+    address: "al gabl al asfer",
+    nationality: "egyption",
+  },
+];
+export default Child;
